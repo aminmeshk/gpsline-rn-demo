cbb6e65c243ac0f7b98399756ca1eeec8122d97b
Amin <amin.meshk@gmail.com>
2020-09-12 10:49:55 +0430

    ✨ Added theme and fonts

--- App.js
+++ App.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './navigation/AppNavigator';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+
+SplashScreen.preventAutoHideAsync();
 
 const App = () => {
+  const [fontsLoaded] = Font.useFonts({
+    shabnam: require('./assets/fonts/Shabnam-FD.ttf'),
+    'shabnam-bold': require('./assets/fonts/Shabnam-Bold-FD.ttf'),
+  });
+
+  if (fontsLoaded) {
+    SplashScreen.hideAsync();
+  } else {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <AppNavigator />
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,26 +1,47 @@
+import { Card, Container, Content, StyleProvider, Text } from 'native-base';
 import React from 'react';
-import { View, Text, StyleSheet, SafeAreaView, StatusBar } from 'react-native';
+import { StyleSheet, StatusBar } from 'react-native';
 import Colors from '../constants/Colors';
+import myAppTheme from '../native-base-theme/variables/myAppTheme';
+import getTheme from '../native-base-theme/components';
 
 const HomeScreen = (props) => {
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor={Colors.primaryDark}
-      />
-      <View>
-        <Text>Hello World!</Text>
-      </View>
-    </SafeAreaView>
+    // <SafeAreaView style={styles.container}>
+    //   <StatusBar
+    //     barStyle="light-content"
+    //     backgroundColor={Colors.primaryDark}
+    //   />
+    //   <View>
+    //     <Text>Hello World!</Text>
+    //   </View>
+    // </SafeAreaView>
+    <StyleProvider style={getTheme(myAppTheme)}>
+      <Container>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor={Colors.primaryDark}
+        />
+        <Content padder contentContainerStyle={styles.content}>
+          <Card style={styles.card}>
+            <Text>سلام دنیا 2!</Text>
+          </Card>
+        </Content>
+      </Container>
+    </StyleProvider>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  content: {
     flex: 1,
     justifyContent: 'center',
+  },
+  card: {
+    justifyContent: 'center',
     alignItems: 'center',
+    paddingHorizontal: 16,
+    paddingVertical: 32,
   },
 });
 
