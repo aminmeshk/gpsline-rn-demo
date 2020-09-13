e426d9aafb48495deece5845a8687f6b523a1178
Amin <amin.meshk@gmail.com>
2020-09-13 20:27:55 +0430

    ❌ Removed expo libraries & unimodules, fixed RTL

--- App.js
+++ App.js
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import { I18nManager } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './navigation/AppNavigator';
-import * as SplashScreen from 'expo-splash-screen';
-import * as Font from 'expo-font';
+// import * as SplashScreen from 'expo-splash-screen';
+// import * as Font from 'expo-font';
 
-SplashScreen.preventAutoHideAsync();
+// SplashScreen.preventAutoHideAsync();
 
 const App = () => {
-  const [fontsLoaded] = Font.useFonts({
-    shabnam: require('./assets/fonts/Shabnam-FD.ttf'),
-    'shabnam-bold': require('./assets/fonts/Shabnam-Bold-FD.ttf'),
-  });
+  // const [fontsLoaded] = Font.useFonts({
+  //   shabnam: require('./assets/fonts/Shabnam-FD.ttf'),
+  //   'shabnam-bold': require('./assets/fonts/Shabnam-Bold-FD.ttf'),
+  // });
 
-  if (fontsLoaded) {
-    SplashScreen.hideAsync();
+  // if (fontsLoaded) {
+  //   SplashScreen.hideAsync();
+  //   I18nManager.forceRTL(true);
+  // } else {
+  //   return null;
+  // }
+
+  useLayoutEffect(() => {
     I18nManager.forceRTL(true);
-  } else {
-    return null;
-  }
+  }, [I18nManager]);
 
   return (
     <NavigationContainer>
--- android/app/src/main/java/com/gpslinerndemo/MainActivity.java
+++ android/app/src/main/java/com/gpslinerndemo/MainActivity.java
@@ -5,18 +5,13 @@ import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.modules.i18nmanager.I18nUtil;
 
-import expo.modules.splashscreen.SplashScreen;
-import expo.modules.splashscreen.SplashScreenImageResizeMode;
-
 public class MainActivity extends ReactActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     I18nUtil sharedI18nUtilInstance = I18nUtil.getInstance();
+    sharedI18nUtilInstance.forceRTL(this, true);
     sharedI18nUtilInstance.allowRTL(this, true);
-    // SplashScreen.show(...) has to be called after super.onCreate(...)
-    // Below line is handled by '@expo/configure-splash-screen' command and it's discouraged to modify it manually
-    SplashScreen.show(this, SplashScreenImageResizeMode.CONTAIN, false);
   }
 
 
